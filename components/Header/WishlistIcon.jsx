793802894e155f461b9d4dbe7e83cec7cfa9b177
merndevreza <merndevreza@gmail.com>
2024-06-05 16:24:52 +0600

    fixing error

--- components/Header/WishlistIcon.jsx
+++ components/Header/WishlistIcon.jsx
@@ -14,7 +14,7 @@ const WishlistIcon = ({dictionary}) => {
         </div>
         <div className="text-xs leading-3">{dictionary.wishlist}</div>
         <div className="absolute right-0 -top-1 w-5 h-5 rounded-full flex items-center justify-center bg-primary text-white text-xs">
-          {wishlist.length}
+          {wishlist?.length}
         </div>
       </div>
     </LinkWithLocale>
