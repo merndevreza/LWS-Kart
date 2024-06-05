a01dddd7d2288fe892c29431fb99aef773720025
merndevreza <merndevreza@gmail.com>
2024-06-05 23:09:42 +0600

    wishlist icon disbaled

--- components/Header/Actions.jsx
+++ components/Header/Actions.jsx
@@ -1,11 +1,11 @@
-import WishlistIcon from "./WishlistIcon";
+// import WishlistIcon from "./WishlistIcon";
 import CartIcon from "./CartIcon";
 import AccountIcon from "./AccountIcon";
 
 const Actions = ({ dictionary }) => {
   return (
     <div className="flex items-center space-x-4">
-      <WishlistIcon dictionary={dictionary} />
+      {/* <WishlistIcon dictionary={dictionary} /> */}
       <CartIcon dictionary={dictionary} />
       <AccountIcon dictionary={dictionary} />
     </div>
