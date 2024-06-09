92d55df7c37871db1ec9a18cc49d715a253ba808
merndevreza <merndevreza@gmail.com>
2024-06-09 14:50:03 +0600

    Header Sticky

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Search from "./Search";
 
 const Header = ({dictionary}) => {
   return (
-    <header className="py-4 shadow-sm bg-white">
+    <header className="py-4 shadow-sm bg-white sticky left-0 top-0 w-full z-50">
       <div className="container flex items-center justify-between">
         <Logo />
         <Search dictionary={dictionary}/>
