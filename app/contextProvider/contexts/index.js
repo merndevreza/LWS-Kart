226fac83ce129395743f816330dcdf3c4e19194a
merndevreza <merndevreza@gmail.com>
2024-06-22 09:22:10 +0600

    env file

--- app/contextProvider/contexts/index.js
+++ app/contextProvider/contexts/index.js
@@ -2,3 +2,4 @@ import { createContext } from "react";
  
 export const AuthContext=createContext()
 export const GuestUserContext=createContext()
+export const ThemeContext=createContext()
---
+++ app/contextProvider/providers/ThemeProvider.jsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useState } from "react";
+import { ThemeContext } from "../contexts";
+
+const ThemeProvider = ({children}) => {
+   const [isDark,setIsDark]=useState(false)
+   return (
+      <ThemeContext.Provider value={{isDark,setIsDark}}>
+         {children}
+      </ThemeContext.Provider>
+   );
+};
+
+export default ThemeProvider;
